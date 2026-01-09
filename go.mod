131ca610fe3003c318aff2590193e164c7557d21
Hadyu Fiqran Hakim <hadyuhakim@gmail.com>
2026-01-09 15:31:43 +0700

    Add ExportService for CSV export functionality and update go.mod/go.sum for dependencies

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Hdeee1/go-implementation
 go 1.25.5
 
 require (
+	github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
---
+++ internal/service/export_service.go
@@ -0,0 +1,57 @@
+package service
+
+import (
+	"bytes"
+	"encoding/csv"
+	"log"
+	"strconv"
+
+	"github.com/Hdeee1/go-implementation/internal/model"
+)
+
+type ExportService struct {}
+
+func NewExportService() *ExportService {
+	return &ExportService{}
+}
+
+func (e *ExportService) ExportToCSV(students []*model.Student) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	write := csv.NewWriter(buf)
+
+	header := []string{"ID", "Name", "Age", "DateOfBirth"}
+	if err := write.Write(header); err != nil {
+		return nil, err
+	}
+
+	progressCh := make(chan int)
+
+	go func() {
+		count := len(students)
+
+		for i, student := range students {
+			row := []string{
+				strconv.Itoa(student.ID),
+				student.Name,
+				strconv.Itoa(student.Age),
+				student.DateOfBirth.Format("2005-10-19"),
+			}
+
+			write.Write(row)
+			progress := (i + 1) * 100 / count
+			progressCh <- progress
+		}
+	}()
+
+	for p := range progressCh {
+		log.Printf("Export progress: %d%%", p)
+	}
+
+	write.Flush()
+
+	if err := write.Error(); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
